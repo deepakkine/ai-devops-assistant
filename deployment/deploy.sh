e6c4025cc89c7e1385246f561bdd8eb35eff7a22
deepakkine <dipakkine24@gmail.com>
2026-07-29 19:31:20 +0530

    refactor(deployment): simplify deployment for ECR-based releases

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
+cd /home/ubuntu/ai-devops-assistant
+
 docker compose pull
+
 docker compose up -d --remove-orphans
 
-docker image prune -f
+docker image prune -af
